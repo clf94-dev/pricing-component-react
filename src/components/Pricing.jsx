1b17d4a2487fe262300a311af7edbe4ac387e79c
clf94-dev <clf94@hotmail.es>
2020-08-28 12:05:42 +0200

    cards added

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
-import {Switch, FormControlLabel, Grid} from '@material-ui/core/';
+import {Switch, Grid} from '@material-ui/core/';
+import {Button} from './Button';
 
 function Pricing() {
     const [checked,
@@ -16,16 +17,62 @@ function Pricing() {
                     <h1 className="pricing-heading">
                         Pricing
                     </h1>
-              
+
                     <Grid component="label" container alignItems="center" spacing={1}>
                         <Grid item>Monthly</Grid>
                         <Grid item>
-                            <Switch checked={checked}              onClick = {
-                        handleClick
-                    } name="checked"/>
+                            <Switch checked={checked} onClick={handleClick} name="checked"/>
                         </Grid>
                         <Grid item>Yearly</Grid>
                     </Grid>
+                    <div className="pricing-container">
+                        <div className="pricing-container-cardInfo">
+                            <h3>Basic</h3>
+                            <h2>$19.99</h2>
+                            <ul className="pricing-container-features">
+                                <hr/>
+                                <li>500GB Storage</li>
+                                <hr/>
+                                <li>2 Users Allowed</li>
+                                <hr/>
+                                <li>Send up to 3GB</li>
+                            </ul>
+                            <Button buttonSize='btn--wide' buttonColor='primary'>
+                                Choose Plan
+                            </Button>
+                        </div>
+                        <div className="pricing-container-cardInfo">
+                            <h3>Professional</h3>
+                            <h2>$24.99</h2>
+                            <ul className="pricing-container-features">
+                                <hr/>
+                                <li>1TB Storage</li>
+                                <hr/>
+                                <li>5 Users Allowed</li>
+                                <hr/>
+                                <li>Send up to 10GB</li>
+                            </ul>
+                            <Button buttonSize='btn--wide'>
+                                Choose Plan
+                            </Button>
+                        </div>
+                        <div className="pricing-container-cardInfo">
+                            <h3>Master</h3>
+                            <h2>$39.99</h2>
+                            <ul className="pricing-container-features">
+                                <hr/>
+                                <li>2 TB Storage</li>
+                                <hr/>
+                                <li>10 Users Allowed</li>
+                                <hr/>
+                                <li>Send up to 20GB</li>
+                            </ul>
+                            <Button buttonSize='btn--wide' >
+                                Choose Plan
+                            </Button>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
